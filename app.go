79ccefe279837b7590c59500cecebd6767b6f04b
morpheuslv <morpheuslv@tencent.com>
2021-01-12 15:32:15 +0800

    feat: add doc create

--- app.go
+++ app.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/zhenligod/my_elasticsearch/elasticsearch"
 )
@@ -11,6 +13,18 @@ func main() {
 		"sort": { "date": "desc"} 
 	}`
 	res, err := elasticsearch.Search(body)
+	defer res.Body.Close()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Println(res)
+
+	doc := map[string]string{
+		"date": time.Now().Format("2006-01-02"),
+	}
+
+	jsonDoc, _ := json.Marshal(doc)
+	res, err = elasticsearch.CreateDoc("5", string(jsonDoc))
 	defer res.Body.Close()
 	if err != nil {
 		log.Fatal(err.Error())
